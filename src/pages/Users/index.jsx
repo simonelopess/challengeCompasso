3603b487c1435b4c347c14c4eef4568ada95f624
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-09 01:41:39 -0300

    feat: Criação da página User

---
+++ src/pages/Users/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Title from '../../components/Title/index';
+
+const Users = () => (
+    <>
+        <Title label="usuários"/>
+    </>
+)
+
+export default Users;
